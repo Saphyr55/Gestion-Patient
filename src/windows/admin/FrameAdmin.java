cd30a93477cf45378f9bacd2a9fd28d8c332822c
Saphyr55 <saphyr0550@gmail.com>
2021-12-16 22:42:22 +0100

    add : ajout de la frame d'admin

--- src/windows/admin/FrameAdmin.java
+++ src/windows/admin/FrameAdmin.java
@@ -1,7 +1,5 @@
 package windows.admin;
 
-import java.util.ArrayList;
-
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -12,43 +10,47 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.time.LocalDate;
-import java.time.Period;
+
+import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.plaf.DimensionUIResource;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.text.MaskFormatter;
 
-import components.ListPatientPanel;
 import hopital.Hopital;
+import hopital.loading.language.LoadingLanguage;
 import hopital.patient.Patient;
 import windows.FrameConnection;
 
+/**
+ * 
+ */
 public class FrameAdmin extends JFrame {
+
+	private static LoadingLanguage loadingLanguage = FrameConnection.getLoadingLanguage();
 
 	/**
 	 * 
 	 */
-	private static final int width = 780;
-	private static final int height = 720;
+	private static final int width = 720;
+	private static final int height = 520;
 	private static final String title = "Admin Gestion";
 	private static boolean isVisible = true;
 
@@ -60,14 +62,40 @@ public class FrameAdmin extends JFrame {
 	/**
 	 * Composant du JPanel de liste de patient
 	 */
-	private ListPatientPanel panelListPatient;
+	private JPanel panelListPatient;
+	private JList<String> listPatient;
+	private JScrollPane listPatientScrollPane;
+	private JButton addPatient;
+	private JTextField foundPatientField;
+	private DefaultListModel<String> namePatients = new DefaultListModel<>();
 	private JPopupMenu popupMenuListPatient;
-	private JMenuItem menuItemAddPatient, menuItemSupprPatient, menuItemAddConsultation;
+	private JMenuItem menuItemAddPatient, menuItemSupprPatient;
 	private ArrayList<String> listNamePatient = new ArrayList<>();
+
+	private String[] namesStringsForTextFields = { (String) loadingLanguage.getJsonObject().get("frame_admin_lastname"),
+			(String) loadingLanguage.getJsonObject().get("frame_admin_firstname"),
+			(String) loadingLanguage.getJsonObject().get("frame_admin_birthday"),
+			(String) loadingLanguage.getJsonObject().get("frame_admin_secu_number"),
+			(String) loadingLanguage.getJsonObject().get("frame_admin_phone"),
+			(String) loadingLanguage.getJsonObject().get("frame_admin_adresse") };
 	private JPanel dataPatientPanel, panelTop, panelCenter, panelBottom;
 	private JTextField[] dataPatientTextFields;
-	private JButton switchLectureModifDataPatientButton;
+	private JTextField patientLastnameInputTextField;
+	private JTextField patientFirstnameInpuTextField;
+	private JFormattedTextField patientDateInputFormattedTextField;
+	private JFormattedTextField patientSecuNumberInputFormattedTextField;
+	private JFormattedTextField patientNumberPhoneInputFormFormattedTextField;
+	private JTextField patientAddressInputTextField;
+	private JButton switchLectureModifDataPatient;
 	private JButton confirmModifButton;
+
+	private static FrameAdminAddPatient frameAdminAddPatient;
+
+	private static MaskFormatter dateFormatter;
+	private static MaskFormatter secuNumbeFormatter;
+	private static MaskFormatter phoneNumberFormatter;
+	private static Font font1 = new Font("SansSerif", Font.BOLD, 20);
+	private static Font font2 = new Font("SansSerif", Font.BOLD, 14);
 
 	/**
 	 * 
@@ -96,7 +124,7 @@ public class FrameAdmin extends JFrame {
 			System.err.println("Failed to initialize LaF");
 		}
 		setSize(width, height);
-		setResizable(false);
+		setResizable(true);
 		setLocationRelativeTo(this);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
@@ -109,12 +137,64 @@ public class FrameAdmin extends JFrame {
 	 * @return liste de patient
 	 */
 	private JPanel setListPatient() {
-		panelListPatient = new ListPatientPanel(height, Hopital.getPatients());
+		/**
+		 * Inititalisation des attribus pour la liste de patient
+		 */
+		panelListPatient = new JPanel(new BorderLayout());
+		panelListPatient.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 0));
+
+		JPanel panelTop = new JPanel(new BorderLayout());
+		addPatient = new JButton("+");
+		foundPatientField = new JTextField();
+
+		/**
+		 * Affichage de la frame pour ajouter un patient lors du clique du bouton '+'
+		 */
+		addPatient.addActionListener(new AddButtonListener());
+
+		/**
+		 * 
+		 */
+		for (int i = 0; i < Hopital.getPatients().size(); i++) {
+			String namePatientString = Hopital.getPatients().get(i).getLastName() + " "
+					+ Hopital.getPatients().get(i).getLastName();
+			namePatients.addElement(namePatientString);
+			listNamePatient.add(namePatientString);
+		}
+		listPatient = new JList<>(namePatients);
+		listPatientScrollPane = new JScrollPane(listPatient);
+
+		/**
+		 * Option text sur le button et autre
+		 */
+		addPatient.setFont(new Font("Sans-Serif", Font.CENTER_BASELINE, 20));
+		foundPatientField.setPreferredSize(new Dimension(0, 20));
+
+		/**
+		 * Dimensions
+		 */
+		panelListPatient.setPreferredSize(new Dimension(200, FrameAdmin.HEIGHT));
+		panelListPatient.setPreferredSize(new Dimension(200, FrameAdmin.HEIGHT));
+		panelListPatient.setMinimumSize(new Dimension(200, FrameAdmin.HEIGHT));
+		panelListPatient.setMaximumSize(new Dimension(200, FrameAdmin.HEIGHT));
+		panelTop.setPreferredSize(new Dimension(0, 40));
+		foundPatientField.setPreferredSize(new Dimension(0, 100));
+
+		/**
+		 * Ajout du button d'ajout de patient et de recherche de patient dans le
+		 * panelTop
+		 * Et ajout du panelTop et de la listPatient dans le panelListPatient
+		 */
+		panelTop.add(addPatient, BorderLayout.WEST);
+		panelTop.add(foundPatientField, BorderLayout.CENTER);
+		panelListPatient.add(panelTop, BorderLayout.NORTH);
+		panelListPatient.add(listPatientScrollPane, BorderLayout.CENTER);
+
 		/*
 		 * Actionne l'affichage de la liste d'ordonnances
 		 * et les données du patient selectionner
 		 */
-		panelListPatient.getListPatient().addMouseListener(new MouseAdapter() {
+		listPatient.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent event) {
 				/**
@@ -132,7 +212,13 @@ public class FrameAdmin extends JFrame {
 					 * selectionner
 					 */
 					if (SwingUtilities.isLeftMouseButton(event)) {
-
+						patientLastnameInputTextField.setText(currentPatient.getLastName());
+						patientFirstnameInpuTextField.setText(currentPatient.getFirstName());
+						patientDateInputFormattedTextField
+								.setText(currentPatient.getBirthday().format(Hopital.FORMATEUR_LOCALDATE));
+						patientAddressInputTextField.setText(currentPatient.getAddress());
+						patientSecuNumberInputFormattedTextField.setText(currentPatient.getSecuNumber());
+						patientNumberPhoneInputFormFormattedTextField.setText(currentPatient.getSecuNumber());
 					}
 
 					/**
@@ -151,11 +237,12 @@ public class FrameAdmin extends JFrame {
 							 * Si la selection est supprimer alors lance showConfimDialog
 							 * pour pouvoir confirmer l'acte de suppression
 							 */
-							panelListPatient.getMenuItemSupprPatient().addActionListener(new ActionListener() {
+							menuItemSupprPatient.addActionListener(new ActionListener() {
 								@Override
 								public void actionPerformed(ActionEvent e) {
 									int input = JOptionPane.showConfirmDialog(null,
-											"frame_medecin_confirm_delete_patient");
+											(String) loadingLanguage.getJsonObject()
+													.get("frame_admin_confirm_delete_patient"));
 
 									/*
 									 * 
@@ -170,14 +257,7 @@ public class FrameAdmin extends JFrame {
 							 * Si la selection est d'ajouter un patient
 							 * affichage une fenetre d'ajout de patient dans la liste de medecin
 							 */
-							panelListPatient.getMenuItemAddPatient().addActionListener(new ActionListener() {
-								@Override
-								public void actionPerformed(ActionEvent e) {
-
-								}
-							});
-
-							panelListPatient.getMenuItemAddConsultation().addActionListener(new ActionListener() {
+							menuItemAddPatient.addActionListener(new ActionListener() {
 								@Override
 								public void actionPerformed(ActionEvent e) {
 
@@ -193,32 +273,92 @@ public class FrameAdmin extends JFrame {
 		});
 
 		/**
-		 * Affichage de la frame pour ajouter un patient lors du clique du bouton '+'
+		 * Trouve le patient ecrit
 		 */
-		panelListPatient.getAddPatient().addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-			}
-
-		});
+		foundPatientField.getDocument().addDocumentListener(new DocumentListenerPatientField());
 
 		return panelListPatient;
-
 	}
 
+	/**
+	 * 
+	 * @return dataPatientPanel
+	 */
 	private JPanel setDataPatient() {
 		dataPatientPanel = new JPanel(new BorderLayout());
-		panelTop = new JPanel();
-		switchLectureModifDataPatientButton = new JButton();
+		panelTop = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		switchLectureModifDataPatient = new JButton(
+				(String) loadingLanguage.getJsonObject().get("frame_admin_switch_mode_modification"));
+		panelTop.add(switchLectureModifDataPatient);
+
+		panelCenter = new JPanel(new BorderLayout());
+		JPanel stringsTextFieldsPanel = new JPanel();
+		JPanel dataPatientTextFieldsPanel = new JPanel();
 		dataPatientTextFields = new JTextField[6];
-		for (int i = 0; i < dataPatientTextFields.length; i++) {
-			dataPatientTextFields[i] = new JTextField();
+		for (int i = 0; i < namesStringsForTextFields.length; i++) {
+			dataPatientTextFields[i] = new JTextField(namesStringsForTextFields[i]);
 			dataPatientTextFields[i].setEditable(false);
-			dataPatientTextFields[i].setPreferredSize(new Dimension(300, 30));
+			dataPatientTextFields[i].setFont(font2);
+			stringsTextFieldsPanel.add(dataPatientTextFields[i]);
 		}
 
+		patientLastnameInputTextField = new JTextField();
+		patientLastnameInputTextField.setEditable(false);
+		patientLastnameInputTextField.setFont(font1);
+		patientFirstnameInpuTextField = new JTextField();
+		patientFirstnameInpuTextField.setEditable(false);
+		patientFirstnameInpuTextField.setFont(font1);
+		patientDateInputFormattedTextField = new JFormattedTextField(dateFormatter);
+		patientDateInputFormattedTextField.setEditable(false);
+		patientDateInputFormattedTextField.setFont(font1);
+		patientSecuNumberInputFormattedTextField = new JFormattedTextField(secuNumbeFormatter);
+		patientSecuNumberInputFormattedTextField.setEditable(false);
+		patientSecuNumberInputFormattedTextField.setFont(font1);
+		patientNumberPhoneInputFormFormattedTextField = new JFormattedTextField(phoneNumberFormatter);
+		patientNumberPhoneInputFormFormattedTextField.setEditable(false);
+		patientNumberPhoneInputFormFormattedTextField.setFont(font1);
+		patientAddressInputTextField = new JTextField();
+		patientAddressInputTextField.setEditable(false);
+		patientAddressInputTextField.setFont(font1);
+
+		dataPatientTextFieldsPanel.add(patientLastnameInputTextField);
+		dataPatientTextFieldsPanel.add(patientFirstnameInpuTextField);
+		dataPatientTextFieldsPanel.add(patientDateInputFormattedTextField);
+		dataPatientTextFieldsPanel.add(patientSecuNumberInputFormattedTextField);
+		dataPatientTextFieldsPanel.add(patientNumberPhoneInputFormFormattedTextField);
+		dataPatientTextFieldsPanel.add(patientAddressInputTextField);
+
+		stringsTextFieldsPanel.setLayout(new BoxLayout(stringsTextFieldsPanel, BoxLayout.PAGE_AXIS));
+		dataPatientTextFieldsPanel.setLayout(new BoxLayout(dataPatientTextFieldsPanel, BoxLayout.PAGE_AXIS));
+
+		panelCenter.add(stringsTextFieldsPanel, BorderLayout.WEST);
+		panelCenter.add(dataPatientTextFieldsPanel, BorderLayout.CENTER);
+
+		panelBottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
+		confirmModifButton = new JButton(
+				(String) loadingLanguage.getJsonObject().get("frame_admin_confirm_modification"));
+		panelBottom.add(confirmModifButton);
+
+		dataPatientPanel.add(panelTop, BorderLayout.NORTH);
+		dataPatientPanel.add(panelCenter, BorderLayout.CENTER);
+		dataPatientPanel.add(panelBottom, BorderLayout.SOUTH);
 		return dataPatientPanel;
+	}
+
+	/**
+	 * Affiche la frame pour ajouter des patients
+	 * Ne peut etre afficher plusieur fois
+	 */
+	private void setFrameAddPatient() {
+		if (frameAdminAddPatient == null)
+			frameAdminAddPatient = new FrameAdminAddPatient();
+
+		frameAdminAddPatient.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent windowEvent) {
+				frameAdminAddPatient = null;
+			}
+		});
 	}
 
 	/**
@@ -248,19 +388,19 @@ public class FrameAdmin extends JFrame {
 	 */
 	private JPopupMenu setPopupMenuOnRigthClickListPatient() {
 		popupMenuListPatient = new JPopupMenu();
-		menuItemAddPatient = new JMenuItem("frame_medecin_popup_add");
-		menuItemSupprPatient = new JMenuItem("frame_medecin_popup_delete");
-		menuItemAddConsultation = new JMenuItem("frame_medecin_popup_add_consultation");
+		menuItemAddPatient = new JMenuItem(
+				(String) loadingLanguage.getJsonObject().get("frame_admin_popup_add"));
+		menuItemSupprPatient = new JMenuItem(
+				(String) loadingLanguage.getJsonObject().get("frame_admin_popup_delete"));
 		popupMenuListPatient.add(menuItemAddPatient);
 		popupMenuListPatient.add(menuItemSupprPatient);
-		popupMenuListPatient.add(menuItemAddConsultation);
 		return popupMenuListPatient;
 	}
 
 	/**
 	 * Le listener du text field pour chercher un patient
 	 */
-	public class DocumentListenerPatientField implements DocumentListener {
+	private class DocumentListenerPatientField implements DocumentListener {
 
 		@Override
 		public void insertUpdate(DocumentEvent e) {
@@ -282,8 +422,33 @@ public class FrameAdmin extends JFrame {
 		 * Et applique filtre model dans notre liste de patient
 		 */
 		private void filter() {
-			String filter = panelListPatient.getFoundPatientField().getText();
-			filterModel((DefaultListModel<String>) panelListPatient.getListPatient().getModel(), filter);
+			String filter = foundPatientField.getText();
+			filterModel((DefaultListModel<String>) listPatient.getModel(), filter);
+		}
+	}
+
+	/**
+	 * 
+	 */
+	private class AddButtonListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			if (frameAdminAddPatient == null) {
+				setFrameAddPatient();
+			}
+		}
+	}
+
+	/**
+	 * 
+	 */
+	public static class CancelButtonFrameAddPatientListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			frameAdminAddPatient.dispose();
+			frameAdminAddPatient = null;
 		}
 	}
 
